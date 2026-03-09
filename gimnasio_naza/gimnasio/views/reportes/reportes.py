92c7d01295667b648f82a72921b7d292113ef991
Fabian Alvarez <sistemasittech@gmail.com>
2026-03-09 10:39:48 -0500

    1

--- gimnasio_naza/gimnasio/views/reportes/reportes.py
+++ gimnasio_naza/gimnasio/views/reportes/reportes.py
@@ -8,62 +8,130 @@ from datetime import datetime
 
 # ====== VISTAS PARA EXPORTAR REPORTES ======
 
-class ExportarRegistrovisitantestemporalesPDF(DjangoView):
+class ExportarAsistenciaPDF(DjangoView):
     """
-    VISTA PARA EXPORTAR CERTIFICACIONES INTERNAS A PDF
-    Obtiene todas las certificaciones internas y las exporta en formato PDF
+    VISTA PARA EXPORTAR CATEGORIAS A PDF
+    Obtiene todas las categorías y las exporta en formato PDF
     """
     
     def get(self, request):
-        # Obtener todas las certificaciones internas
-        registro = Registrovisitantestemporales.objects.all()
+        # Obtener todas las categorias 
+        asistencia = Asistencia.objects.all()
         
         # Definir las columnas que se muestran en el reporte
-        columnas = ['ID', 'fecha_registro' ]
+        columnas = ['ID','Fecha de asistencia','Hora de ingreso','Hora de salida','Membresia','Documento de usuario']
         
         # Preparar los datos en formato de tuplas
         datos = [
-            (visitante.id, visitante.fecha_registro, )
-            for visitante in registro
+            (a.id,a.fecha_asistencia,a.hora_ingreso,a.hora_salida,(a.fk_membresia.fk_usuario.nombre_usuario,a.fk_membresia.fk_usuario.apellido_usuario),a.fk_membresia.fk_usuario.documento)
+            for a in asistencia
         ]
         
         # Generar nombre del archivo con timestamp
-        nombre_archivo = f'Registrovisitantestemporales{datetime.now().strftime("%d_%m_%Y")}'
+        nombre_archivo = f'Reporte_Asistencia_{datetime.now().strftime("%d_%m_%Y")}'
         
         # Llamar funcion de exportacion a PDF
         return exportar_pdf(
-            titulo='REPORTE DE CERTIFICACIONES INTERNAS',
+            titulo='REPORTE DE ASISTENCIAS',
             columnas=columnas,
             datos=datos,
             nombre_archivo=nombre_archivo
         )
 
 
-class ExportarRegistrovisitantestemporalesExcel(DjangoView):
+class ExportarAsistenciaExcel(DjangoView):
     """
     VISTA PARA EXPORTAR CATEGORIAS A EXCEL
     Obtiene todas las categorias y las exporta en formato Excel
     """
     
     def get(self, request):
-        # Obtener todas las certificaciones internas
-        registro = Registrovisitantestemporales.objects.all()
+        # Obtener todas las categorias 
+        asistencia = Asistencia.objects.all()
         
-        # Definir las columnas que se mostraran en el reporte
-        columnas = ['ID', 'fecha_registro']
+        # Definir las columnas que se muestran en el reporte
+        columnas = ['ID', 'Fecha de asistencia', 'Hora de ingreso', 'Hora de salida', 'Membresia','Documento de usuario']
         
-        # Preparar los datos en  tuplas
+        # Preparar los datos en formato de tuplas
         datos = [
-            (visitante.id, visitante.fecha_registro, )
-            for visitante in registro
+            (a.id,a.fecha_asistencia,a.hora_ingreso,a.hora_salida,(a.fk_membresia.fk_usuario.nombre_usuario,a.fk_membresia.fk_usuario.apellido_usuario),a.fk_membresia.fk_usuario.documento)
+            for a in asistencia
         ]
         
         # Generar nombre del archivo con timestamp
-        nombre_archivo = f'Registrovisitantestemporales{datetime.now().strftime("%d_%m_%Y")}'
+        nombre_archivo = f'Reporte_Asistencia_{datetime.now().strftime("%d_%m_%Y")}'
         
-        # Llamar funcion de exportacion a Excel
+        # Llamar funcion de exportacion a PDF
         return exportar_excel(
-            titulo='REPORTE DE REGISTRO DE VISITANTES TEMPORALES',
+            titulo='REPORTE DE ASISTENCIAS',
+            columnas=columnas,
+            datos=datos,
+            nombre_archivo=nombre_archivo
+        )
+
+
+# ====== VISTAS PARA EXPORTAR REPORTES MEMBRESIA======
+
+class ExportarMembresiaPDF(DjangoView):
+    """
+    VISTA PARA EXPORTAR CATEGORIAS A PDF
+    Obtiene todas las categorías y las exporta en formato PDF
+    """
+    
+    def get(self, request):
+        # Obtener todas las categorias 
+        membresia = Membresia.objects.all()
+        
+        # Definir las columnas que se 
+        # 
+        # en el reporte
+        columnas = ['ID','Fecha de inicio','Fecha de fin','Membresia','Documento de usuario']
+        
+        # Preparar los datos en formato de tuplas
+        datos = [
+            (m.id,m.fecha_inicio, m.fecha_fin,(m.fk_usuario.nombre_usuario,m.fk_usuario.apellido_usuario),m.fk_usuario.documento)
+            for m in membresia
+        ]
+        
+        # Generar nombre del archivo con timestamp
+        nombre_archivo = f'Reporte_membresia_{datetime.now().strftime("%d_%m_%Y")}'
+        
+        # Llamar funcion de exportacion a PDF
+        return exportar_pdf(
+            titulo='REPORTE DE MEMBRESIAS',
+            columnas=columnas,
+            datos=datos,
+            nombre_archivo=nombre_archivo
+        )
+
+
+class ExportarMembresiaExcel(DjangoView):
+    """
+    VISTA PARA EXPORTAR CATEGORIAS A EXCEL
+    Obtiene todas las categorias y las exporta en formato Excel
+    """
+    
+    def get(self, request):
+        # Obtener todas las categorias 
+        membresia = Membresia.objects.all()
+        
+        # Definir las columnas que se 
+        # 
+        # en el reporte
+        columnas = ['ID','Fecha de inicio','Fecha de fin','Membresia','Documento de usuario']
+        
+        # Preparar los datos en formato de tuplas
+        datos = [
+            (m.id,m.fecha_inicio, m.fecha_fin,(m.fk_usuario.nombre_usuario,m.fk_usuario.apellido_usuario),m.fk_usuario.documento)
+            for m in membresia
+        ]
+        
+        # Generar nombre del archivo con timestamp
+        nombre_archivo = f'Reporte_membresia_{datetime.now().strftime("%d_%m_%Y")}'    
+        
+        # Llamar funcion de exportacion a PDF
+        return exportar_excel(
+            titulo='REPORTE DE MEMBRESIAS',
             columnas=columnas,
             datos=datos,
             nombre_archivo=nombre_archivo
