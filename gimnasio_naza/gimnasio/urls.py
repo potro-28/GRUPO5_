cf736508fa56b7fcd3a60a69367f3437e2c9fc01
david santiago nino <davidsantigo02n@gmail.com>
2026-03-09 07:43:22 -0500

    vvv

--- gimnasio_naza/gimnasio/urls.py
+++ gimnasio_naza/gimnasio/urls.py
@@ -46,16 +46,16 @@ urlpatterns = [
     path('crear_asistencia/', AsistenciaCreateView.as_view(), name='crear_asistencia'),
     path('editar_asistencia/<int:pk>/', AsistenciaUpdateView.as_view(), name='editar_asistencia'),
     path('eliminar_asistencia/<int:pk>/', AsistenciaDeleteView.as_view(), name='eliminar_asistencia'),
-    path('exportar_asistencia_pdf', ExportarAsistenciaPDF.as_view(), name='exportar_asistencia_pdf'),
-    path('exportar_asistencia_excel', ExportarAsistenciaExcel.as_view(), name='exportar_asistencia_excel'),
+    #==path('exportar_asistencia_pdf', ExportarAsistenciaPDF.as_view(), name='exportar_asistencia_pdf'),
+    #==path('exportar_asistencia_excel', ExportarAsistenciaExcel.as_view(), name='exportar_asistencia_excel'),
     #Membresias
     path('listar_membresia/', MembresiaListView.as_view(), name='listar_membresia'),
     path('crear_membresia/', MembresiaCreateView.as_view(), name='crear_membresia'),
     path('editar_membresia/<int:pk>/', MembresiaUpdateView.as_view(), name='editar_membresia'),
     path('eliminar_membresia/<int:pk>/', MembresiaDeleteView.as_view(), name='eliminar_membresia'),
-    path('exportar_membresia_pdf', ExportarMembresiaPDF.as_view(), name='exportar_membresia_pdf'),
-    path('exportar_membresia_excel', ExportarMembresiaExcel.as_view(), name='exportar_membresia_excel'),
-    
+    #==path('exportar_membresia_pdf', ExportarMembresiaPDF.as_view(), name='exportar_membresia_pdf'),
+    #==path('exportar_membresia_excel', ExportarMembresiaExcel.as_view(), name='exportar_membresia_excel'),
+
     #Notificaciones
     path('listar_notificacion/', NotificacionListView.as_view(), name='listar_notificacion'),
     path('crear_notificacion/', NotificacionCreateView.as_view(), name='crear_notificacion'),
